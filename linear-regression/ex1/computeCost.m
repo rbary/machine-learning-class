3b773fd365a6728d43b8f3b3451fd9d2791306d8
dubary <bary.roland@gmail.com>
2017-12-27 13:08:51 +0100

    submit linear regression with one var

--- linear-regression/ex1/computeCost.m
+++ linear-regression/ex1/computeCost.m
@@ -6,16 +6,14 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
+% You need to return the following variables correctly
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
-
-
+h = X * theta;
+sqrErr = (h-y) .^ 2;
+J = 1/(2 * m) * sum(sqrErr);
 
 % =========================================================================
 
--- linear-regression/ex1/gradientDescent.m
+++ linear-regression/ex1/gradientDescent.m
@@ -17,11 +17,12 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+    h = X * theta;
+    temp_theta1 = theta(1) - alpha/m * sum((h-y) .* X(:,1));
+    temp_theta2 = theta(2) - alpha/m * sum((h-y) .* X(:,2));
+    
+    theta(1) = temp_theta1;
+    theta(2) = temp_theta2;
 
     % ============================================================
 
